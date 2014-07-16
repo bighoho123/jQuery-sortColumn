7a53c289146eef757d234edb5a5765ad95a61a4d
Jinzhe Li <jinjinwudi@gmail.com>
2014-07-16 17:08:42 +0930

    Update the format options
    dd-mm-yyyy
    dd/mm/yyyy
    number
    string
    currency

--- jQuery_sortColumn.js
+++ jQuery_sortColumn.js
@@ -3,37 +3,81 @@
  * @author Jinzhe Li (jinjinwudi@gmail.com)
  */
 jQuery.fn.sortColumn = function (options) {
+	var $table = this;
 
+	// Get the default options
     var opts = jQuery.extend({}, jQuery.fn.sortColumn.defaults, options);
-    function sortFunction(index) {
+    var order = opts.order;
+    var index = opts.index;
+    var format = opts.format;
+
+    // Define the comparator function
+    function sortFunction(index,format) {
         return function (a, b) {
             var dataA=jQuery(a).children("td:nth-child("+index+")").html();
             var dataB=jQuery(b).children("td:nth-child("+index+")").html();
             
-            if (dataA==""){
-                dataA='01-01-1970';
-            }
-            if (dataB==""){
-                dataB='01-01-1970';
-            }
+            if (format=='dd-mm-yyyy'){
+	            if (dataA==""){
+	                dataA='01-01-1970';
+	            }
+	            if (dataB==""){
+	                dataB='01-01-1970';
+	            }
+	            
+	            dataA=dataA.split('-');
+	            dataB=dataB.split('-');
+	            
+	            dataA=new Date(dataA[2],dataA[1]-1,dataA[0]).getTime();
+	            dataB=new Date(dataB[2],dataB[1]-1,dataB[0]).getTime();
+
+	            if (dataA>dataB) 
+	                return 1;
+	            else if (dataA==dataB)
+	                return 0;
+	            else 
+	                return -1;
+	        }
+
+	        if (format=='dd/mm/yyyy'){
+	            if (dataA==""){
+	                dataA='01/01/1970';
+	            }
+	            if (dataB==""){
+	                dataB='01/01/1970';
+	            }
+	            
+	            dataA=dataA.split('/');
+	            dataB=dataB.split('/');
+	            
+	            dataA=new Date(dataA[2],dataA[1]-1,dataA[0]).getTime();
+	            dataB=new Date(dataB[2],dataB[1]-1,dataB[0]).getTime();
+
+	            if (dataA>dataB) 
+	                return 1;
+	            else if (dataA==dataB)
+	                return 0;
+	            else 
+	                return -1;
+	        }
+
+	        if (format=='number'){
+	        	return dataA-dataB;
+	        }
+
+	        if (format=='currency'){
+	        	dataA=dataA.replace(/$/g,'');
+	        	dataA=dataA.replace(/,/g,'');
+	        	dataB=dataB.replace(/$/g,'');
+	        	dataB=dataB.replace(/,/g,'');
+	        	return dataA-dataB;
+	        }
+
             
-            dataA=dataA.split('-');
-            dataB=dataB.split('-');
-            
-            dataA=new Date(dataA[2],dataA[1]-1,dataA[0]).getTime();
-            dataB=new Date(dataB[2],dataB[1]-1,dataB[0]).getTime();
-            if (dataA>dataB) 
-                return 1;
-            else if (dataA==dataB)
-                return 0;
-            else 
-                return -1;
         };
     }
 
-    var $table = this;
-    var order = opts.order;
-    var index = opts.index;
+    
     
     var original=[];
     var sorte=[];
@@ -41,7 +85,7 @@ jQuery.fn.sortColumn = function (options) {
         original.push(ele);
     });
     
-    sorted = original.sort(sortFunction(index));
+    sorted = original.sort(sortFunction(index,format));
     if (order=='desc')
         sorted=sorted.reverse();
     $table.children("tbody").html(sorted);
@@ -51,6 +95,7 @@ jQuery.fn.sortColumn = function (options) {
 // Default parameter
 jQuery.fn.sortColumn.defaults = {
     index: 1,
-    order: 'asc'
+    order: 'asc',
+    format: 'string'
 };
 
